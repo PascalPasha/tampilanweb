9c33ac4a1b64f548cd8e662a63d3e557c1e815a6
PascalPasha <testiing1212@gmail.com>
2021-03-15 19:23:27 +0700

    tampilanweb

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pascal Pahlevi Pasha|20190140105</title>
+</head>
+<style>
+    body{
+        background-color: white;
+        color: black;
+    }
+</style>
+<body>
+    
+    <div class="produk">
+        <center><img style="width: 100px" src="images/logo.png" 
+            alt="ini adalah alternatif img"></center>
+        <br><br><br>
+        <center><img style="width: 300px;" 
+            src="images/mark_ryden.jpg" alt=""></center>
+
+        <h1>Product</h1>
+        <h3>Mark Ryden Tas Ransel Laptop Travel Backpack Anti Thief 17 Inch - MR-TW6656 - Gray</h3>
+        <h5>Ready 17 inch </h5>
+        <h2>Rp575.500</h2>
+        <div class="button">
+            <button aria-label="Beli Sekarang" class="ncss-btn-primary-dark">
+                <a href="https://www.tokopedia.com/kusumawicitra/mark-ryden-tas-ransel-laptop-backpack-anti-thief-17-inch-gray?trkid=f%3DCa0000L000P0W0S0Sh%2CCo0Po0Fr0Cb0_src%3Dsearch_page%3D9_ob%3D23_q%3Dtas+laptop+17_bmexp%3D0_po%3D27_catid%3D3905_bmexp%3D0&whid=0">Buy Now</a></button>
+            
+        </div>
+        <div class="deskripsi barang" class="detail deskripsi" itemprop="deskripsi">
+            <p>
+                Feature:
+            </p>
+            <ul>
+                <li>
+                    Featuring the bag is equipped with an "octopus suction cup" feature and a strap on the sleeve of the bag to make it easier to carry your cellphone or mp3 player comfortably.
+                </li>
+
+            </ul>
+
+        </div>
+        
+    </div>
+</body>
+</html>
